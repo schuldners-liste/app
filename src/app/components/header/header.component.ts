8da6a89f69a118036d2aa4eeda019d1ef772ce8e
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-22 11:26:00 +0200

    #10 implement header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  text: string[] = [];
 
-  ngOnInit(): void {
+  constructor(private header: HeaderService) {
   }
 
+  ngOnInit(): void {
+    this.header.text.subscribe(text => {
+      this.text = text;
+    });
+  }
 }
