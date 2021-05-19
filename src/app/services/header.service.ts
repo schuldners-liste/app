39998eb04a514a64571cc248e81d7d7e6fdace0f
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-19 17:02:20 +0200

    #9 implement header text as subject

--- src/app/services/header.service.ts
+++ src/app/services/header.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
 
-  private headerText: string[] = [];
+  private readonly headerText: Subject<string>;
 
   constructor() {
+    this.headerText = new Subject<string>();
   }
 
-  public set(newText: string): void {
-    this.headerText[0] = newText.substring(0, 20);
+  public set text(newText: string) {
+    this.headerText.next(newText.substring(0, 20));
   }
 
-  public get text(): Observable<string[]> {
-    return of(this.headerText);
+  public getText(): Observable<string> {
+    return this.headerText;
   }
 }
