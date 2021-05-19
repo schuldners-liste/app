0d93e79de2774c3dc9fb436911787f5d1264f3ea
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-19 17:08:58 +0200

    #15 get redirectUrl form url

--- src/app/views/join/join.component.ts
+++ src/app/views/join/join.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-join',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinComponent implements OnInit {
 
-  constructor() { }
+  private redirectUrl: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private auth: AuthService) {
+    this.redirectUrl = '';
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(queryParams => {
+      this.redirectUrl = queryParams.redirectUrl ? queryParams.redirectUrl : '/entries';
+    });
   }
 
 }
