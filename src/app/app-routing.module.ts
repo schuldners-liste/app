31baee9e4bba799150242e143cfae47fb4e4ec37
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-19 17:12:01 +0200

    #21 implement default, entries and join route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth-guard/auth.guard';
 
 const routes: Routes = [
-  {path: 'join', loadChildren: () => import('./views/join/join.module').then(m => m.JoinModule)}
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'entries'
+  },
+  {
+    path: 'join',
+    loadChildren: () => import('./views/join/join.module').then(m => m.JoinModule)
+  },
+  {
+    path: 'entries',
+    loadChildren: () => import('./views/entries/entries.module').then(m => m.EntriesModule),
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
