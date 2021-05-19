280f273f243672c44a39386952d5c5917b27cce8
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-19 17:14:45 +0200

    perform code rollback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +7,4 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   title = 'schuldners-liste';
-  loggedIn = false;
-
-  constructor(private auth: AuthService) {
-    // auth.signInWithEmailAndPasswort('dev@dorfingerjonas.at', 'Developer123!').then(value => {
-    //   console.log(value);
-    // });
-
-    auth.signOut().then(() => {
-      // auth.googleAuth();
-    });
-  }
 }
