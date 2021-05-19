ea70490f7b601ebc05652e740d847b4922774fc3
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-19 17:07:17 +0200

    #19 generate login-form component

--- src/app/views/join/join.module.ts
+++ src/app/views/join/join.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { JoinComponent } from './join.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
   {path: '', component: JoinComponent}
 ];
 
 @NgModule({
-  declarations: [JoinComponent],
+  declarations: [JoinComponent, LoginFormComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
