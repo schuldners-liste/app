56309143ae308e625d4b0081874e6d3ce244074a
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-08-09 12:18:50 +0200

    #20 implement login form control

--- src/app/views/join/login-form/login-form.component.ts
+++ src/app/views/join/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  emailControl: FormControl;
+  passwordControl: FormControl;
+
+  constructor(private auth: AuthService) {
+    this.emailControl = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.passwordControl = new FormControl('', [
+      Validators.required
+    ]);
+  }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    if (this.emailControl.valid && this.passwordControl.valid) {
+      this.auth.signInWithEmailAndPassword(this.emailControl.value, this.passwordControl.value).then(res => {
+        console.log(res);
+      }).catch(err => {
+        console.log(err.message);
+      });
+    } else {
+      console.log('foisch');
+    }
+  }
 }
