67f5073d83f9a9457df10cc3ad2bf7ee499c050a
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-08-09 12:22:53 +0200

    #20 import forms modules

--- src/app/views/join/join.module.ts
+++ src/app/views/join/join.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { JoinComponent } from './join.component';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -13,6 +14,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class JoinModule {
