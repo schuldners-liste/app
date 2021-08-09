3ef28763123b82ed912d53604e11f0dcca337a08
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-08-09 12:25:41 +0200

    #25 handle user state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'schuldners-liste';
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              public auth: AuthService) {
+  }
+
+  ngOnInit(): void {
+    let initDone = false;
+
+    this.auth.isLoggedIn.subscribe(state => {
+      let redirectUrl = '/entries';
+
+      if (state) {
+        this.route.queryParams.subscribe(value => {
+          if (value.redirectUrl) {
+            redirectUrl = value.redirectUrl;
+          }
+        });
+      } else if (initDone) {
+        redirectUrl = '/join';
+      }
+
+      this.router.navigateByUrl(redirectUrl, { replaceUrl: true })
+        .catch(err => {
+          console.error(err);
+        });
+
+      initDone = true;
+    });
+  }
 }
