67000410e38f31322e22035b0242f1406e80bce8
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-09-20 15:33:54 +0200

    #22 set header text to 'Entries' when entries component inits

--- src/app/views/entries/entries.component.ts
+++ src/app/views/entries/entries.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../services/header.service';
 
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.component.html',
-  styleUrls: ['./entries.component.scss']
+  styleUrls: [ './entries.component.scss' ]
 })
 export class EntriesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private header: HeaderService) {
   }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.header.text = 'Entries';
+    });
+  }
 }
