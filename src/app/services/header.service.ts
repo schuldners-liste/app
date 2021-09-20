5df45f9492d5569505f4b0c05ab976ac814bff33
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-09-20 15:30:05 +0200

    #9 refactor methods and provide current string

--- src/app/services/header.service.ts
+++ src/app/services/header.service.ts
@@ -6,17 +6,24 @@ import { Observable, Subject } from 'rxjs';
 })
 export class HeaderService {
 
+  private currentText: string;
   private readonly headerText: Subject<string>;
 
   constructor() {
+    this.currentText = '';
     this.headerText = new Subject<string>();
   }
 
-  public set text(newText: string) {
-    this.headerText.next(newText.substring(0, 20));
+  public get text(): string {
+    return this.currentText;
   }
 
-  public getText(): Observable<string> {
+  public set text(newText: string) {
+    this.currentText = newText.substring(0, 20);
+    this.headerText.next(this.currentText);
+  }
+
+  public getTextAsObservable(): Observable<string> {
     return this.headerText;
   }
 }
