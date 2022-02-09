2e87707b3eaf35af20310f3183c023e810425766
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-09 17:41:49 +0100

    implement detail view

---
+++ src/app/views/debtors/detail/detail.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { DebtorService } from '../../../services/debtor.service';
+import { Debtor, Entry } from '../../../models/models';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeaderService } from '../../../services/header.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditComponent } from '../edit/edit.component';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: [ './detail.component.scss' ]
+})
+export class DetailComponent implements OnInit {
+
+  public debtor: Debtor | null;
+  public entries: Entry[];
+  public searchCondition: string;
+
+  constructor(private readonly headerService: HeaderService,
+              private readonly debtorService: DebtorService,
+              private readonly route: ActivatedRoute,
+              private readonly router: Router,
+              private readonly dialog: MatDialog) {
+    this.debtor = null;
+    this.searchCondition = '';
+    this.entries = [];
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.debtorService.getDebtorByName(params.name).subscribe(debtor => {
+        if (debtor) {
+          this.debtor = debtor;
+
+          this.debtor.entries = this.debtor.entries.filter(e => !e.deleted);
+          this.entries = this.debtor.entries;
+
+          if (this.debtor.entries.length === 0) {
+            this.router.navigateByUrl('/debtors');
+          }
+
+          setTimeout(() => {
+            this.headerService.text = `Entries of ${ debtor.name }`;
+          });
+        }
+      });
+    });
+  }
+
+  openDialog(entry: Entry): void {
+    this.dialog.open(EditComponent, {
+      data: entry
+    }).afterClosed().subscribe(result => {
+      if (result && this.debtor) {
+        this.debtorService.update(this.debtor.name, result);
+      }
+    });
+  }
+
+  public delete(entry: Entry): void {
+    if (this.debtor) {
+      this.debtorService.delete(this.debtor.name, entry.id);
+    }
+  }
+
+  public getEntryStateText(entry: Entry): string {
+    const stateParts: string[] = [];
+
+    if (entry.restored) {
+      stateParts.push('restored');
+    }
+
+    if (entry.edited) {
+      stateParts.push('edited');
+    }
+
+    return `(${ stateParts.join(' & ') })`;
+  }
+
+  public searchConditionChange(): void {
+    if (this.debtor) {
+      this.entries = this.debtor?.entries.filter(e => {
+        return !!(e.object?.toLowerCase().includes(this.searchCondition.toLowerCase())
+          || e.date.toLowerCase().includes(this.searchCondition.toLowerCase())
+          || e.reason.toLowerCase().includes(this.searchCondition.toLowerCase())
+          || e.amount.toString().toLowerCase().includes(this.searchCondition.toLowerCase()));
+      }) || [];
+    }
+  }
+}
