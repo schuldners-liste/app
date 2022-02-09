3bfae597c3dc053fcd4b2b5cad1ca637f349d921
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-09 17:30:04 +0100

    update debtors module

---
+++ src/app/views/debtors/debtors.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DebtorsComponent } from './debtors.component';
+import { MatIconModule } from '@angular/material/icon';
+import { DetailComponent } from './detail/detail.component';
+import { HelpersModule } from '../../helpers/helpers.module';
+import { EditComponent } from './edit/edit.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DebtorsComponent
+  },
+  {
+    path: ':name',
+    component: DetailComponent
+  }
+];
+
+@NgModule({
+  declarations: [ DebtorsComponent, DetailComponent, EditComponent ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatIconModule,
+    MatDialogModule,
+    HelpersModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule
+  ]
+})
+export class DebtorsModule {
+}
