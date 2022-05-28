869eed57f3cc9a2dee7ce00039f60bb5919586fc
dorfingerjonas <contact@dorfingerjonas.at>
2022-05-28 10:50:52 +0200

    import components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoadingComponent,
-    FooterComponent
+    FooterComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,9 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     FontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSnackBarModule
   ],
   providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy } ],
   exports: [],
