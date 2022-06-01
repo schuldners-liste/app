485179d2c1e3c67e3abc9da515b9a0c03fe667c1
dorfingerjonas <contact@dorfingerjonas.at>
2022-06-01 20:05:44 +0200

    create nav component

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: [ './nav.component.scss' ]
+})
+export class NavComponent implements OnInit {
+
+  constructor(private readonly auth: AuthService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  signOut(): void {
+    this.auth.signOut();
+  }
+}
